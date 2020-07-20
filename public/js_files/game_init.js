4229579e0910fbd95c419bcff2b70c9afee56d55
Emanuel Siu <e@emanuelsiu.com>
2020-07-19 18:47:36 -0700

    scene lit, about to work on loader

--- public/js_files/game_init.js
+++ public/js_files/game_init.js
@@ -9,6 +9,8 @@ import chestGLTF from '../THREE/Assets/gltf_files/Treasure.gltf'
 import gsap from 'gsap';
 
 
+
+
 // Setting up the environment with 'THREE.js' (name of library not a count) assets
 // export const init = (item) => {
 
@@ -17,8 +19,7 @@ import gsap from 'gsap';
 
 //-------- LOADING MANAGER---------
 // passing this manager into 3D assets as parameter
-const loaderCounter = document.getElementById('loader');
-// loaderCounter.innerHTML = 'hello';
+const startingScreen = document.getElementById('loader');
 const manager = new THREE.LoadingManager();
 manager.onStart = (url, itemsLoaded, itemsTotal) => {
     console.log(`Started loading file: ${url} . Loaded ${itemsLoaded} of ${itemsTotal} files`)
@@ -28,7 +29,7 @@ manager.onLoad = () => {
 }
 manager.onProgress = (url, itemsLoaded, itemsTotal) => {
     // console.log(`loading file: ${url} . Loaded ${itemsLoaded} of ${itemsTotal} files`)
-    loaderCounter.innerHTML = `${itemsLoaded / itemsTotal * 100} %`;
+    startingScreen.innerHTML = `${itemsLoaded / itemsTotal * 100} %`;
 }
 manager.onError = (url) => {
     console.log(`there was an error with ${url}`)
@@ -40,14 +41,14 @@ const scene = new THREE.Scene();
 const nearClippingPlane = 0.06
 const farClippingPlane = 10000
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, nearClippingPlane, farClippingPlane);
-camera.position.set(0, 100, 300);
+camera.position.set(0, 80, 250);
 
 //--------------------- RENDERER SETTINGS -----------------------------------------
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 // enable shadow maps
 renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+// renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 // enable hdr settings
 // renderer.physicallyCorrectLights = true;
 // renderer.toneMapping = THREE.ACESFilmicToneMapping;
@@ -56,31 +57,59 @@ const container = document.querySelector(".scene"); //<-- our DOM Reference to H
 container.appendChild(renderer.domElement);
 
 //--------------------- LIGHT SETTINGS -----------------------------------------
-//------  DIRECTION LIGHT 1
-// const directionLight = new THREE.DirectionalLight(0xFFFFFF, 1);
-// directionLight.position.set(0, 100, 100)
-// scene.add(directionLight);
+//------ "Sun" Light
+const directionLight = new THREE.DirectionalLight(0xFFFFFF, 2);
+directionLight.position.set(-50, 100, 100)
+directionLight.castShadow = true;
+directionLight.shadow.mapSize.width = 1024;
+directionLight.shadow.mapSize.height = 1024;
+directionLight.shadow.camera.near = 0.01;
+directionLight.shadow.camera.far = 10000
+directionLight.shadow.bias = - 0.000001;
 
-// const dirLightHelper = new THREE.DirectionalLightHelper(directionLight, 7)
-// scene.add(dirLightHelper)
+scene.add(directionLight);
 
-// var ambientLight = new THREE.AmbientLight(0x404040, 20); // soft white light
-// scene.add(ambientLight);
+// ---hint of ambient light
+var ambientLight = new THREE.AmbientLight(0x404040, 4); // soft white light
+scene.add(ambientLight);
 
-//------- SPOT LIGHT 1
-const spotLight = new THREE.SpotLight(0xFFFFFF, 80, 400, 0.5, 0.8, 2.0) //color,intensity,distance,angle(radian),penumbra(0.0-1.0),decay(realistic =2)
-spotLight.position.set(100, 300, 110)
-spotLight.castShadow = true;
-spotLight.shadow.mapSize.width = 1024;
-spotLight.shadow.mapSize.height = 1024;
-spotLight.shadow.camera.near = 0.01;
-spotLight.shadow.camera.far = 10000
-spotLight.shadow.bias = - 0.000001;
-scene.add(spotLight)
-
-// const sphereSize = 20;
-// const lightHelper = new THREE.SpotLightHelper(spotLight, sphereSize)
-// scene.add(lightHelper)
+//------- DOOR SPOT LIGHTS
+// left light
+const doorLight_01 = new THREE.SpotLight(0xFFFFFF, 100, 400, 0.2, 1.0, 2.0) //color,intensity,distance,angle(radian),penumbra(0.0-1.0),decay(realistic =2)
+doorLight_01.position.set(-200, 370, 80)
+doorLight_01.target.position.set(-200, 40, -25)
+doorLight_01.castShadow = true;
+doorLight_01.shadow.mapSize.width = 1024;
+doorLight_01.shadow.mapSize.height = 1024;
+doorLight_01.shadow.camera.near = 0.01;
+doorLight_01.shadow.camera.far = 10000
+doorLight_01.shadow.bias = - 0.000001;
+scene.add(doorLight_01)
+scene.add(doorLight_01.target)
+// middle light
+const doorLight_02 = new THREE.SpotLight(0xFFFFFF, 100, 400, .2, 1.0, 2.0) //color,intensity,distance,angle(radian),penumbra(0.0-1.0),decay(realistic =2)
+doorLight_02.position.set(0, 370, 80)
+doorLight_02.target.position.set(0, 40, -25)
+doorLight_02.castShadow = true;
+doorLight_02.shadow.mapSize.width = 1024;
+doorLight_02.shadow.mapSize.height = 1024;
+doorLight_02.shadow.camera.near = 0.01;
+doorLight_02.shadow.camera.far = 10000
+doorLight_02.shadow.bias = - 0.000001;
+scene.add(doorLight_02)
+scene.add(doorLight_02.target)
+// right light
+const doorLight_03 = new THREE.SpotLight(0xFFFFFF, 100, 400, .2, 1.0, 2.0) //color,intensity,distance,angle(radian),penumbra(0.0-1.0),decay(realistic =2)
+doorLight_03.position.set(200, 370, 80)
+doorLight_03.target.position.set(200, 40, -25)
+doorLight_03.castShadow = true;
+doorLight_03.shadow.mapSize.width = 1024;
+doorLight_03.shadow.mapSize.height = 1024;
+doorLight_03.shadow.camera.near = 0.01;
+doorLight_03.shadow.camera.far = 10000
+doorLight_03.shadow.bias = - 0.000001;
+scene.add(doorLight_03)
+scene.add(doorLight_03.target)
 
 // -----------SPOT LIGHT 2
 const spotLight2 = new THREE.SpotLight(0xFFFFFF, 50, 300, 0.5, 0.8, 2.0) //color,intensity,distance,angle(radian),penumbra(0.0-1.0),decay(realistic =2)
@@ -93,19 +122,14 @@ spotLight2.shadow.camera.far = 10000
 spotLight2.shadow.bias = - 0.000001;
 
 
-
-const sphereSize2 = 20;
-const lightHelper2 = new THREE.SpotLightHelper(spotLight2, sphereSize2)
-
-
 //------ POINT LIGHT
-const pointLight = new THREE.PointLight(0xffffff, 5, 160, 1.5); //color, intensity, distance, decay
-pointLight.position.set(50, 75, -100);
-scene.add(pointLight);
+// const pointLight = new THREE.PointLight(0xffffff, 5, 160, 1.5); //color, intensity, distance, decay
+// pointLight.position.set(50, 75, -100);
+// scene.add(pointLight);
 
-const pointLightHelperSize = 25;
-const pointLightHelper = new THREE.PointLightHelper(pointLight, pointLightHelperSize, 0x000000);
-scene.add(pointLightHelper);
+// const pointLightHelperSize = 25;
+// const pointLightHelper = new THREE.PointLightHelper(pointLight, pointLightHelperSize, 0x000000);
+// scene.add(pointLightHelper);
 
 
 //--------------------- CUSTOM FLOOR SETTINGS-----------------------------------
@@ -120,13 +144,12 @@ scene.add(floorMesh);
 
 
 //--------------------- 3D ASSET LOADER SETTINGS-----------------------------------
-let MainDoor1, MainDoor2
-let Frame1, Frame2
+
 const loader = new GLTFLoader(manager);
-let doorMain;
-let doorFrame;
+let Door_01, Door_02, Door_03;
 let loaded = false
-// load door
+// --LOADING DOOR AND ASSIGNING ALL 3 TO A DOOR GROUP
+let DoorGroup = new THREE.Group();
 loader.load(doorGLTF, (gltf) => {
     // assign shadow casting and recieving to all meshes in gltf object
     gltf.scene.traverse((node) => {
@@ -135,22 +158,19 @@ loader.load(doorGLTF, (gltf) => {
             node.receiveShadow = true;
         }
     })
-    doorMain = gltf.scene.children[1];
-    MainDoor1 = doorMain.clone();
-    MainDoor2 = doorMain.clone();
-    MainDoor1.position.set(200, 0, 0);
-    MainDoor2.position.set(-200, 0, 0);
-    scene.add(MainDoor1);
-    scene.add(MainDoor2);
-    doorFrame = gltf.scene.children[0];
-    Frame1 = doorFrame.clone();
-    Frame2 = doorFrame.clone();
-    Frame1.position.set(200, 0, 0);
-    Frame2.position.set(-200, 0, 0);
-    scene.add(Frame1);
-    scene.add(Frame2);
+    Door_01 = gltf.scene;
+    Door_02 = Door_01.clone();
+    Door_03 = Door_01.clone();
+    Door_02.position.set(200, 0, 0);
+    Door_03.position.set(-200, 0, 0);
+    DoorGroup.add(Door_01);
+    DoorGroup.add(Door_02);
+    DoorGroup.add(Door_03);
+    DoorGroup.position.set(-50, 0, 0); //move all doors together with this group.
+    // scene.add(Door_01);
+    // scene.add(Door_02);
+    scene.add(DoorGroup);
     loaded = true;
-    scene.add(gltf.scene);
     animate(loaded);
 },
     (xhr) => {
@@ -170,7 +190,7 @@ loader.load(chestGLTF, (gltf) => {
         }
     })
     TreasureChest = gltf.scene.children[0];
-    TreasureChest.position.set(75, 0, -150);
+    // TreasureChest.position.set(75, 0, -150);
     scene.add(gltf.scene);
     spotLight2.target = TreasureChest;
     scene.add(spotLight2)
